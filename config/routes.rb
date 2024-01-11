76bd5a4e08bab94082288e95aafc0594e580cb64
INVSTWS <tutiputiygi@gmail.com>
2024-01-11 19:05:15 +0200

    added: new background, welcome page, some styles to form partial

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   resources :characters
   resources :animes
+
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  get "welcome", to: "animes#welcome"
+  root "animes#index"
 end
