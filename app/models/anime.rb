a6fa7577400ca64cb9dad5c708b75345784e6719
INVSTWS <tutiputiygi@gmail.com>
2024-01-11 14:52:58 +0200

    associations to all models

--- app/models/anime.rb
+++ app/models/anime.rb
@@ -1,2 +1,4 @@
 class Anime < ApplicationRecord
+    has_many :anime_characters
+    has_many :characters, through: :anime_characters
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -1,2 +1,4 @@
 class Character < ApplicationRecord
+    has_many :anime_characters
+    has_many :animes, through: :anime_characters
 end
